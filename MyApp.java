21d85c0228418d35463d68044c064a97d181c4f1
lala <lala@example.com>
2021-12-24 04:05:54 +0000

    #1-3 Hello World

---
+++ MyApp.java
@@ -0,0 +1,14 @@
+public class MyApp {
+
+    // comment
+    /*
+    comment
+    comment
+    comment
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
