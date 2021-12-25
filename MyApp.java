e33f4f6ec2314aabc144e72fbf12b83e2ba1ed1e
lala <lala@example.com>
2021-12-25 05:43:49 +0000

    #5 データ型

--- MyApp.java
+++ MyApp.java
@@ -1,14 +1,28 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // 変数の宣言
-        // 型 変数名
-        // String msg;
-        // msg = "Hello World Again!";
-        String msg = "Hello World Again!";
+        // 文字 : char
+        char a = 'a'; // シングルクォーテーション！
 
-        // System.out.println("Hello World");
+        // 整数 : byte, short, int, long
+        int x = 10;
+        long y = 55555555555L; // 数値の末尾にL(or l)が必要
+
+        // 浮動小数点数 : float, double
+        double d = 23423.344;
+        float f = 32.33F; // 数値の末尾にF(or f)が必要
+
+        // 論理値 : boolean
+        boolean flag = true; // or false
+
+        // 文字列 : String
+        // String msg = "Hello World Again!"; // ダブルクォーテーション！
+        String msg = "Hello Wo\nrld Again\t!";
         System.out.println(msg);
+
+        // 特殊文字（Stringにて使用可能）
+        // \n : 改行
+        // \t : タブ
     }
 
 }
