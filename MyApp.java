f32fb071bb78b3a4881d9ce98eb8c3562bb93161
lala <lala@example.com>
2021-12-25 05:35:27 +0000

    #4 変数

--- MyApp.java
+++ MyApp.java
@@ -1,14 +1,14 @@
 public class MyApp {
 
-    // comment
-    /*
-    comment
-    comment
-    comment
-    */
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // 変数の宣言
+        // 型 変数名
+        // String msg;
+        // msg = "Hello World Again!";
+        String msg = "Hello World Again!";
+
+        // System.out.println("Hello World");
+        System.out.println(msg);
     }
 
 }
