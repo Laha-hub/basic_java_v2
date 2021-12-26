0810e4c9a5e5c151c1842c6e93b7a9899dc01d2d
lala <lala@example.com>
2021-12-26 08:55:19 +0000

    #10 while

--- MyApp.java
+++ MyApp.java
@@ -1,24 +1,21 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // switch
+        // while
 
-        String signal = "green";
-        switch (signal) {
-            case "red":
-                System.out.println("stop!");
-                break;
-            case "blue":
-            case "green":
-                System.out.println("go!");
-                break;
-            case "yellow":
-                System.out.println("caution!");
-                break;
-            default:
-                System.out.println("wrong signal!");
-                break;
-        }
+        // 0 - 9の数値を表示するプログラム
+        // int i = 100;
+        // while (i < 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // do while
+        int i = 100;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
 
 
     }
