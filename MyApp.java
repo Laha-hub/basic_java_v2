d5278d79544bb3a8fc8b497cc11f56f05bb788cf
lala <lala@example.com>
2021-12-26 08:50:23 +0000

    #9 switch

--- MyApp.java
+++ MyApp.java
@@ -1,23 +1,25 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // if
+        // switch
 
-        // 比較演算子 : >, >=, <, <=, ==, !=
-        // 論理演算子 : &&, ||, !
+        String signal = "green";
+        switch (signal) {
+            case "red":
+                System.out.println("stop!");
+                break;
+            case "blue":
+            case "green":
+                System.out.println("go!");
+                break;
+            case "yellow":
+                System.out.println("caution!");
+                break;
+            default:
+                System.out.println("wrong signal!");
+                break;
+        }
 
-        int score = 90;
-        // if (score > 80) {
-        //     System.out.println("Great!");
-        // } else if (score > 60) {
-        //     System.out.println("Good!");
-        // } else {
-        //     System.out.println("so so ... !");
-        // }
-
-        // 条件演算子を用いて if/else 記述をスッキリ
-        String msg = score > 80 ? "Great!" : "so so ... !";
-        System.out.println(msg);
 
     }
 
