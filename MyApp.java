fea476ac990dac5399fbaa2132770b27e8eddf16
lala <lala@example.com>
2021-12-27 07:18:27 +0000

    #14 データ型とメモリの関係

--- MyApp.java
+++ MyApp.java
@@ -1,25 +1,13 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // 配列の操作
+        // データ型とメモリの関係
 
-        int[] sales = {700, 400, 500};
+        // データ型の区分
+        // 基本データ型（プリミティブ型） : byte, short, int, long, float, double, boolean, char
+        // 参照型 : String, Array, Class
 
-        // for (int i = 0; i < 3; i++) {
-        //     System.out.println(sales[i]); // 700, 400, 500
-        // }
-
-        // 配列の要素数 : sales.length
-        // for (int i = 0; i < sales.length; i++) {
-        //     System.out.println(sales[i]); // 700, 400, 500
-        // }
-
-        // forのスッキリした記述
-        for (int sale : sales) {
-            System.out.println(sale); // 700, 400, 500
-        }
-
-
+        // プログラムは基本的にコンピュータのメモリ上に展開される
     }
 
 }
