6ca840dfc7c7d6089a675a0fa76bbd07848fdad4
lala <lala@example.com>
2021-12-28 09:26:36 +0000

    #18 class

--- MyApp.java
+++ MyApp.java
@@ -1,30 +1,25 @@
+// class : 複雑なデータ型を自分で作りたいときに便利
+
+// 例：何らかのアプリ作成をしており、ユーザーに関するデータ型が欲しい場合
+// String s;のようにUser tom;として、様々なデータを持った変数を宣言できると便利 => classでできる
+
+
+class User {
+    // フィールド : classに属する変数
+    String name = "Me!";
+
+    void sayHi() {
+        System.out.println("Hi!");
+    }
+}
+
 public class MyApp {
-
-    // methodのoverload
-
-    public static void sayHi(String name) {
-        // method内で使う変数の有効範囲（スコープ）: xやnameはsayHi()のブロック内{}でのみ有効
-        int x = 10;
-        System.out.println("Hi! " + name);
-    }
-
-
-
-    // overload : Javaでは同じ名前のmethodを複数記述可能
-    public static void sayHi() {
-        System.out.println("Hi! Nobody!");
-    }
-
-
-
     public static void main(String[] args) {
-        // xとnameはmain()のスコープ範囲外
-        // System.out.println(name); // Error!
-        // System.out.println(x); // Error!
+        // Classは参照型なので使用するには、インスタンス化が必要
+        User tom; // データ型の指定
+        tom = new User(); // インスタンス化
 
-        // overload
-        sayHi("Steve"); // Hi! Steve
-        sayHi(); // Hi! Nobody!
+        System.out.println(tom.name); //Me!
+        tom.sayHi(); // Hi!
     }
-
 }
