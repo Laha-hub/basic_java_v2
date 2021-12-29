4d0fa311a05cec97091d063bf9dddf6e784304be
lala <lala@example.com>
2021-12-29 12:09:01 +0000

    #24 getter, setter

--- MyApp.java
+++ MyApp.java
@@ -1,44 +1,35 @@
-// パッケージ
-// クラスをパッケージに含めるには、パッケージ名のようなフォルダ作成の必要がある
-// 例 : myapp.dotinstall.comの場合 => com/dotinstall/myapp
+// getter, setter
 
-// class User {
-//     String name;
-//     User(String name) {
-//         this.name = name;
-//     }
-//     User() {
-//         this("Me!");
-//     }
-//     void sayHi() {
-//         System.out.println("Hi! " + this.name);
-//     }
-// }
+// Javaでは1つのソースファイルにつき、publicなクラスは1つしか持てないので、Userクラスはpackage privateにする
 
+class User {
+    private String name;
+    private int score;
 
-// class AdminUser extends User {
-//     AdminUser(String name) {
-//         super(name);
-//     }
-//     void sayHello() {
-//         System.out.println("Hello! " + this.name);
-//     }
-//     @Override
-//     void sayHi() {
-//         System.out.println("[admin] Hi! " + this.name);
-//     }
-// }
+    public User(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    // scoreを参照するためのメソッド（getter）
+    public int getScore() {
+        return this.score;
+    }
+
+    // scoreの値を更新するためのメソッド（setter）
+    public void setScore(int score) {
+        if (score > 0) {
+            this.score = score;
+        }
+    }
+}
 
 
-// public class MyApp {
-//     public static void main(String[] args) {
-//         User tom = new User("Tom");
-//         System.out.println(tom.name);
-//         tom.sayHi();
-
-//         AdminUser bob = new AdminUser("Bob");
-//         System.out.println(bob.name);
-//         bob.sayHi();
-//         bob.sayHello();
-//     }
-// }
+public class MyApp {
+    public static void main(String[] args) {
+        User tom = new User("Tom", 65); // (score = 65)
+        tom.setScore(85); // (score = 85)
+        tom.setScore(-22); // (score = 85) (-22は無視)
+        System.out.println("score = " + tom.getScore()); // score = 85
+    }
+}
--- com/dotinstall/myapp/MyApp.java
+++ com/dotinstall/myapp/MyApp.java
@@ -1,16 +1,6 @@
-// パッケージのコンパイル
-// 別のパッケージのクラスを読み込むには、パッケージ名を指定する必要がある
-// import文を使うと他のパッケージ読み込みを指定でき、記述を簡略化できる
-
-// コンパイルは、基点となるmain()メソッドがあるクラスをコンパイルすると、関連・使用しているクラスも連動してコンパイルされる
-// javac com/dotinstall/myapp/MyApp.java => エラーなければOK
-// java com.dotinstall.myapp.MyApp で実行
-
-
 package com.dotinstall.myapp;
-import com.dotinstall.myapp.model.User; // Userクラスのimport
-import com.dotinstall.myapp.model.AdminUser; // AdminUserクラスのimport
-// import com.dotinstall.myapp.model.*; // * : ワイルドカード指定も可能
+import com.dotinstall.myapp.model.User;
+import com.dotinstall.myapp.model.AdminUser;
 
 public class MyApp {
     public static void main(String[] args) {
