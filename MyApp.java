a8011d7d0bb2ca9a78f35ca343e9b18338452c1d
lala <lala@example.com>
2021-12-30 11:00:52 +0000

    #29 interface

--- MyApp.java
+++ MyApp.java
@@ -1,37 +1,38 @@
-// 抽象クラス
+// interface
 
-// 抽象クラス : 他のクラスに継承してもらうことを前提にしているクラス。それ自身をインスタンス化できない。
-// 具象クラス : 抽象クラスを継承したクラス
+// interface : クラスの機能を拡張する仕組み
+// interfaceは継承と異なり、１つのクラスにいくつでも適用させることができるので、柔軟にクラスの機能拡張が可能となる。
 
 
+// 印刷機能を各クラスに提供するインターフェース Printableを作る
+interface Printable {
+    // 定数（public, static, finalを省略可能）
+    double VERSION = 1.2;
 
-// 抽象クラス User から JapaneseUserクラスと AmericanUserクラスを生成する
-abstract class User {
-    public abstract void sayHi(); // 抽象メソッド : 具象クラスにてメソッドの内容を実装する
-}
+    // 抽象メソッド（public, abstractを省略可能）
+    void print();
 
-// 具象クラス
-class JapaneseUser extends User {
-    @Override
-    public void sayHi() {
-        System.out.println("こんにちは！");
+    // defaultメソッド : 抽象メソッドと異なりここに直接メソッドの内容を実装する
+    public default void getInfo() {
+        System.out.println("I/F ver. " + Printable.VERSION);
     }
+
+    // staticメソッド : このinterfaceに属するメソッド（defaultメソッドが複雑になったとき、ヘルパー的に使用したりする）
 }
 
-// 具象クラス
-class AmericanUser extends User {
+// interfaceを使用するには「implements」キーワードをつける
+class User implements Printable {
     @Override
-    public void sayHi() {
-        System.out.println("Hi!");
+    public void print() {
+        System.out.println("Now printing user profile...");
     }
 }
 
 
 public class MyApp {
     public static void main(String[] args) {
-        AmericanUser tom = new AmericanUser();
-        JapaneseUser taro = new JapaneseUser();
-        tom.sayHi(); // Hi!
-        taro.sayHi(); // こんにちは！
+        User tom = new User();
+        tom.print(); // Now printing user profile...
+        tom.getInfo(); // I/F ver. 1.2
     }
 }
