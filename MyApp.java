28d647b5581bd9dcdcf5b1369b42bc41b9072cf1
lala <lala@example.com>
2021-12-31 11:46:44 +0000

    #35 無名クラス、ラムダ式

--- MyApp.java
+++ MyApp.java
@@ -1,35 +1,50 @@
-// スレッド処理 : Thread
-
-// スレッド : コンピュータの処理単位のこと
-// スレッドを複数立ち上げれば、複数の処理を同時に実行可能となる
-// 任意で作成したスレッドの処理内容はRunnableというインターフェースを実装したクラスを記述し、new Thread()に対して渡せばOK
+// 無名クラス、ラムダ式
 
 
-class MyRunnable implements Runnable {
-    // Runnableの抽象メソッドは「run()」
-    @Override
-    public void run() {
-        for (int i = 0; i < 500; i++) {
-            System.out.print('*');
-        }
-    }
-}
+// 関数型インターフェース : 抽象メソッドを１つだけ持つインターフェースのことを入力に対して、出力が１つだけに定まる
+// class MyRunnable implements Runnable {
+//     @Override
+//     public void run() {
+//         for (int i = 0; i < 500; i++) {
+//             System.out.print('*');
+//         }
+//     }
+// }
 
+
+// Javaのバージョン8から関数型インターフェースは「ラムダ式」という特殊な記法で置き換えることが可能となった
+
+// ラムダ式の構文
+// (引数) -> {処理}
 
 
 
 public class MyApp {
     public static void main(String[] args) {
-        // 別のスレッド作成
-        MyRunnable r = new MyRunnable();
-        Thread t = new Thread(r);
-        t.start(); // * 500個
+        // MyRunnable r = new MyRunnable();
+        // Thread t = new Thread(r);
+        // t.start();
+
+        // new Thread(new MyRunnable()).start(); // new MyRunnable()部分を無名クラスにする（以下）
+
+        // new Thread(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         for (int i = 0; i < 500; i++) {
+        //             System.out.print('*');
+        //         }
+        //     }
+        // }).start();
+
+        // ラムダ式にて書き換え
+        new Thread(() -> {
+            for (int i = 0; i < 500; i++) {
+                System.out.print('*');
+            }
+        }).start();
 
         for (int i = 0; i < 500; i++) {
             System.out.print('.');
         }
-         // . 500個
-
-         // .と*はそれぞれ混ざり合って表示される。各スレッドが同時に実行されているため。
     }
 }
