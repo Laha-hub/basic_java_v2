e3632326a06b8e9c357cc91a7b17514db94bb5f4
lala <lala@example.com>
2021-12-31 11:36:19 +0000

    #34 スレッド処理

--- MyApp.java
+++ MyApp.java
@@ -1,42 +1,35 @@
-// ジェネリクス : generics
+// スレッド処理 : Thread
 
-// ジェネリクス : 汎用化されたデータ型で、クラスやインターフェースを作成することができる
-
-
-// ジェネリクス不使用の場合
-// class MyInteger {
-//     public void getThree(int x) {
-//         System.out.println(x);
-//         System.out.println(x);
-//         System.out.println(x);
-//     }
-// }
+// スレッド : コンピュータの処理単位のこと
+// スレッドを複数立ち上げれば、複数の処理を同時に実行可能となる
+// 任意で作成したスレッドの処理内容はRunnableというインターフェースを実装したクラスを記述し、new Thread()に対して渡せばOK
 
 
-// getThree()をintだけでなく、doubleやStringでも行いたい場合、修正が面倒。
-// ジェネリクスを用いれば、引数のデータ型を汎用化することができる
-
-// ジェネリクス使用の場合
-// <T>を記述することで、どのデータ型にも対応可能となる
-class MyData<T> {
-    public void getThree(T x) {
-        System.out.println(x);
-        System.out.println(x);
-        System.out.println(x);
+class MyRunnable implements Runnable {
+    // Runnableの抽象メソッドは「run()」
+    @Override
+    public void run() {
+        for (int i = 0; i < 500; i++) {
+            System.out.print('*');
+        }
     }
 }
 
+
+
+
 public class MyApp {
     public static void main(String[] args) {
-        // ジェネリクス不使用の場合
-        // MyInteger mi = new MyInteger();
-        // mi.getThree(55); // 55, 55, 55
+        // 別のスレッド作成
+        MyRunnable r = new MyRunnable();
+        Thread t = new Thread(r);
+        t.start(); // * 500個
 
-        // ジェネリクス使用の場合
-        MyData<Integer> i = new MyData<>(); // <>に指定する型は参照型しか指定できない
-        i.getThree(32); // 32, 32, 32
-        MyData<String> s = new MyData<>();
-        s.getThree("Hello"); // Hello, Hello, Hello
+        for (int i = 0; i < 500; i++) {
+            System.out.print('.');
+        }
+         // . 500個
 
+         // .と*はそれぞれ混ざり合って表示される。各スレッドが同時に実行されているため。
     }
 }
