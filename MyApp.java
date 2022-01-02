52cb24985d57292ba219b27e748aab15c5ebbc11
lala <lala@example.com>
2022-01-02 13:15:10 +0000

    #41 HashMap

--- MyApp.java
+++ MyApp.java
@@ -1,43 +1,52 @@
-// HashSet
+// HashMap
 
-// HashSet : ArrayList同様、複数のデータを扱うもの。重複を許さない点とデータを保持する順番が定まらない点が異なる。
-// このほか、HashSetと似たようなクラスで、「TreeSet」クラス, 「LinkedHashSet」クラスがある。
-// これらもHashSetと操作は同じだが、データをお保持する順番が異なる。
-// HashSet : 要素の順番不定
-// TreeSet : 要素の値順にソートされる
-// LinkedHashSet : 要素の追加された順
-
+// HashMap : 複数のデータを管理するためのもの。keyとvalueでデータを管理する。
+// このほか、HashMapと似たようなクラスに「TreeMap」クラス、「LinkedHashMap」クラスがある。
+// これらはデータの操作については同じだが、データが保持される順番が異なる。
+// HashMap : データの順番不定
+// TreeMap : keyの順番
+// LinkedHashMap : データが追加された順番
 
 
 import java.util.*;
 public class MyApp {
     public static void main(String[] args) {
-        // HashSet<Integer> sales = new HashSet<>();
-        Set<Integer> sales = new HashSet<>();
+        // HashMap<String, Integer> sales = new HashMap<>(); // keyがString, valueがInteger
+        Map<String, Integer> sales = new HashMap<>();
 
-        // 要素の追加 : add()
-        sales.add(10);
-        sales.add(20);
-        sales.add(30);
-        sales.add(10); // HashSetとSetクラスは重複を許さないので、追加されない
-        // また、HashSetとSetクラスは要素の順番が定まらないので、何番目の要素を取り出すということもできない
+        // 要素の追加 : put()
+        sales.put("tom", 10);
+        sales.put("bob", 20);
+        sales.put("steve", 30);
 
+        // 要素の取得 : get("key")
         // 要素の個数 : size()
-        System.out.println(sales.size()); // 3 (重複を許さないため)
+        System.out.println(sales.get("tom")); // 10
+        System.out.println(sales.size()); // 3
 
-        // すべての要素を取り出す（for文）
-        for (Integer sale : sales) {
-            System.out.println(sale); // すべての要素を取り出せるが、順番が固定されない
+
+        // すべての要素取り出し
+        for (Map.Entry<String, Integer> sale : sales.entrySet()) {
+            System.out.println(sale.getKey() + ": " + sale.getValue()); // tom: 10, bob: 20, steve: 30
         }
 
-        // 要素の削除 : remove()
-        sales.remove(30);
 
-        // すべての要素を取り出す（for文）
-        for (Integer sale : sales) {
-            System.out.println(sale); // すべての要素を取り出せるが、順番が固定されない
+        // 値の更新
+        sales.put("tom", 100);
+
+        // 要素の削除 : remove("key")
+        sales.remove("steve");
+
+        for (Map.Entry<String, Integer> sale : sales.entrySet()) {
+            System.out.println(sale.getKey() + ": " + sale.getValue()); // tom: 100, bob: 20
         }
+
     }
 }
+
+
+
+
+
 
 
